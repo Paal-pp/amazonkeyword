36c13ffde2d8ec6f5a8d61e449b5b19a25dab09c
paal <paal98@outlook.com>
2022-12-21 09:19:49 +0800

    挂代理 增加品牌占有率和自然占有率

--- api/cleaner.py
+++ api/cleaner.py
@@ -24,8 +24,6 @@ class AmazonCleaner:
         items_data=[]
         ad_data=[]
         items = tree.xpath('//div[contains(@class,"s-result-list")]/div[@data-asin]')
-        ad_NO=0
-        items_NO=0
         if len(items) == 0:
             res['code'] = 201
             res['msg'] = '列表为空'
@@ -36,20 +34,19 @@ class AmazonCleaner:
                 judge_res= self.judge_item(item)
                 if judge_res =='yes':
                     item_id= self.goodsListFieldCleaner(item, 'itemId')
-                    items_NO=items_NO+1
-                    solo_item={
-                        "itemId":item_id,
-                        "itemNo": items_NO
+                    item_title = self.goodsListFieldCleaner(item, 'title')
+                    if "Muzata" in item_title:
+                        IS = 1
+                    else:
+                        IS =0
+                    solo_item = {
+                        "item_id":item_id,
+                        "IS":IS
                     }
                     items_data.append(solo_item)
                 elif judge_res =="adyes":
                     aditem_id= self.goodsListFieldCleaner(item, 'itemId')
-                    ad_NO=ad_NO+1
-                    solo_aditem={
-                        "aditem_id":aditem_id,
-                        "adNO":ad_NO,
-                    }
-                    ad_data.append(solo_aditem)
+                    ad_data.append(aditem_id)
                 else:
                     continue
             res_datadic={
@@ -105,161 +102,10 @@ class AmazonCleaner:
             result = item.xpath('.//img[@class="s-image"]/@src')
             result = result[0] if result else ''
 
-        elif type == 'badge':
-            data_asin = item.xpath('./@data-asin')[0]
-            result = item.xpath(f'.//div[@class="sg-row"][1]//span[@id="{data_asin}-label"]//span/text()')
-            result = ''.join([i.strip('\n').strip() for i in result])
-
-        elif type == 'sponsored':
-            # result = item.xpath(
-            #     './/div[@class="a-section a-spacing-none a-spacing-top-small"]/div[@class="a-row a-spacing-micro"]/span[@class="a-size-base a-color-secondary"]/text()')
-            result = item.xpath(
-                './/div[contains(@class, "a-section a-spacing-none")]/div[@class="a-row a-spacing-micro"]/span[@class="a-size-base a-color-secondary"]/text()')
-            result = result[0] if result else ''
-
         elif type == 'title':
             result = item.xpath('.//span[contains(@class, "a-color-base a-text-normal")]/text()')
             result = result[0] if result else ''
 
-        elif type == 'by':
-            result = item.xpath('string(.//h2/following-sibling::div[@class="a-row a-size-base a-color-secondary"])')
-            result = re.sub(' +', ' ', result.replace('\n', '').strip())
-
-        elif type == 'url':
-            result = item.xpath('.//h2/a[@class="a-link-normal a-text-normal"]/@href')
-            if result:
-                result = 'https://www.amazon.com' + result[0]
-            else:
-                result = ''
-
-        elif type == 'rateInfo':
-            rating = item.xpath('.//span[@class="a-icon-alt"]/text()')
-            rate_num = item.xpath('.//a[@class="a-link-normal"]/span[@class="a-size-base"]/text()')
-            rate_link = item.xpath('.//div[@class="a-row a-size-small"]//a[@class="a-link-normal"]/@href')
-            itemId = item.xpath('./@data-asin')[0] if item.xpath('./@data-asin') else ''
-
-            result = {
-                'rating': rating[0] if rating else '',
-                'rateNum': rate_num[0] if rate_num else '',
-                'rateLink': f'https://www.amazon.com/dp/{itemId}/{rate_link[0]}' if rate_link else ''
-            }
-
-        elif type == 'priceInfo':
-            if not clean_for_special:
-                new_item = item.xpath('.//div[@class="a-section a-spacing-none a-spacing-top-small"]')
-                if new_item:
-                    item = new_item[0]
-                else:
-                    return result
-
-            result = item.xpath(
-                './/a[@class="a-size-base a-link-normal s-no-hover a-text-normal"]//span[@class="a-offscreen"]/text()')
-            price_range_tag = item.xpath('.//span[@class="a-price-dash"]/text()')
-            if price_range_tag:
-                result = {'current': '-'.join(result)}
-            else:
-                keys = ['current', 'original']
-                result = dict(zip(keys, result))
-                # 单价处理
-                unit_price = item.xpath('.//span[@class="a-price"]/following-sibling::span/text()')
-                if unit_price:
-                    result['byUnit'] = unit_price[0]
-
-            # 若价格为空，则取'更多购买选择'中的价格
-            if not result:
-                more_buying_choices = self.goodsListFieldCleaner(item, 'moreBuyingChoices')
-                if more_buying_choices:
-                    choice_info = more_buying_choices['title']
-                    current_price = re.search('(US\$[\d.,]*) ', choice_info)
-                    result['current'] = current_price.group(1) if current_price else ''
-
-        elif type == 'shipping':
-            result = item.xpath('.//span[@class="a-size-small a-color-secondary"]/text()')
-            result = result[0] if result else ''
-
-        elif type == 'stockInfo':
-            if not clean_for_special:
-                new_item = item.xpath(
-                    './/div[@class="a-section a-spacing-none a-spacing-top-micro"]/div[@class="a-row a-size-base a-color-secondary"]/span/@aria-label')
-                if not new_item:
-                    return result
-
-            result = item.xpath('.//div[@class="a-row a-size-base a-color-secondary"]/span/@aria-label')
-            result = result[0].strip() if result else ''
-
-        elif type == 'moreBuyingChoices':
-            if not clean_for_special:
-                new_item = item.xpath('.//div[@class="a-section a-spacing-none a-spacing-top-mini"]')
-                if new_item:
-                    item = new_item[0]
-                else:
-                    return result
-
-            more_buying_choices = item.xpath(
-                './/div[@class="a-row a-size-base a-color-secondary"]/span[@class="a-size-base a-color-secondary"]/..')
-            title_root = ''
-            if more_buying_choices:
-                node_title_root = more_buying_choices[0].xpath('.//span[@class="a-size-base a-color-secondary"]')
-                if node_title_root:
-                    title_root = node_title_root[0].xpath('string(.)')
-                    more_buying_choices[0].remove(node_title_root[0])
-
-                title = more_buying_choices[0].xpath('string(.)')
-                link = more_buying_choices[0].xpath('.//a[@class="a-link-normal"]/@href')
-                result = {
-                    'title_root': title_root.replace('\n', '').strip(),
-                    'title': re.sub(' +', ' ', title.replace('\n', '').strip()),
-                    'link': f'https://www.amazon.com{link[0]}' if link else ''
-                }
-
-        elif type == 'subscribeSave':
-            result = item.xpath(
-                './/div[@class="a-section a-spacing-none a-spacing-top-small"]/div[@class="a-row a-size-small a-color-secondary"]/span/text()')
-            result = [i for i in result if i.strip('\n').strip() != '']
-            result = result[0] if result else ''
-
-        elif type == 'couponSave':
-            result = item.xpath(
-                './/div[@class="a-section a-spacing-none a-spacing-top-small"]//span[@class="s-coupon-unclipped "]/span/text()')
-            result = ' '.join([i.strip('\n').strip() for i in result])
-
-        elif type == 'speciesType':
-            result = item.xpath('string(.//a[@class="a-size-base a-link-normal a-text-bold"])')
-            result = result.replace('\n', '').strip()
-
-        elif type == 'otherSpecies':
-            otherSpecies = []
-            results = item.xpath('//div[@class="a-row"]/div[@class="a-row a-spacing-mini"]')
-            for result in results:
-                species_type = self.goodsListFieldCleaner(result, 'speciesType')
-                price_info = self.goodsListFieldCleaner(result, 'priceInfo', clean_for_special=True),
-                stock_info = self.goodsListFieldCleaner(result, 'stockInfo', clean_for_special=True),
-                more_buying_choices = self.goodsListFieldCleaner(result, 'moreBuyingChoices', clean_for_special=True),
-
-                species = {
-                    'speciesType': species_type,
-                    'priceInfo': price_info,
-                    'stockInfo': stock_info[0] if stock_info else '',
-                    'moreBuyingChoices': more_buying_choices[0] if more_buying_choices else '',
-                }
-
-                otherSpecies.append(species)
-
-            result = otherSpecies
-
-        elif type == 'otherVersions':
-            other_versions = []
-            versions = item.xpath(
-                './/div[@class="a-row a-spacing-top-micro a-size-small a-color-base"]/a[@class="a-size-small a-link-normal"]')
-            for version in versions:
-                title = version.xpath('./text()')
-                link = version.xpath('./@href')
-                other_versions.append({
-                    'title': title[0].replace('\n', '').strip() if title else '',
-                    'link': f'https://www.amazon.com{link[0]}' if link else '',
-                })
-
-            result = other_versions
         return result
 
     def htmlCaptchaCleaner(self,data):
@@ -316,29 +162,6 @@ class AmazonCleaner:
                 j += 1
         return inf_list
 
-    def mark_share(self,data):
-        myself_item=0
-        myself_aditem =0
-        item_list= data['itemlist']
-        aditem_list =data['adlist']
-        for solo_item in item_list:
-            select_sql="""
-                        select 
-            """
-            if select_sql == 1:
-                myself_item=myself_item+1
-        item_markshare =myself_item/len(item_list)
-        aditem_list = data['adlist']
-        for solo_aditem in aditem_list:
-            select_sql = """
-                                select 
-                    """
-            if select_sql == 1:
-                aditem_list = aditem_list + 1
-        aditem_markshare = aditem_list / len(aditem_list)
-        data['item_markshare']=item_markshare
-        data['aditem_markshare']=aditem_markshare
-        return data
 
 
 
--- api/main.py
+++ api/main.py
@@ -1,18 +1,38 @@
+import pymysql as mysql
+import requests as requests
+
+from tools.agentUtil import *
+from time import strftime
+import datetime
+import random
+import os
+from test import *
+from cleaner import *
 from api.cleaner import *
 from func_timeout import func_timeout, exceptions
-import json, re
+import json
+import re
 from tools.data import *
 from tools.decorators import *
 import requests
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
+bgsched = BackgroundScheduler()
 requests.packages.urllib3.disable_warnings()
-import random
-from tools.agentUtil import *
+global base_url, db, cur
+db = mysql.connect(
+    host="127.0.0.1",
+    port=3306,
+    user="root",
+    passwd="Mld901",
+    db="advertising_distribution")
+cur = db.cursor()
+
 
 
 class AmazonCrawler:
     def __init__(self):
         self.dataCleaner = AmazonCleaner()
-
 
         self.request_time_out = 60
         self.session = None
@@ -25,15 +45,22 @@ class AmazonCrawler:
         """
         task_type = taskDetail['type']
         if task_type == 'amazonKeyword':
-            return self.getGoodsByKeyword(self.getParam(taskDetail, 'keyword'),
-                                          self.getParam(taskDetail, 'page',),
-                                          self.getParam(taskDetail, 'sort', 'featured_rank'),
-                                          self.getParam(taskDetail, 'countryCode', ),
-                                          self.getParam(taskDetail, 'zipCode', ''),
-                                          self.getParam(taskDetail, 'language',))
+            return self.getGoodsByKeyword(
+                self.getParam(taskDetail, 'keyword'),
+                self.getParam(taskDetail, 'page',),
+                self.getParam(taskDetail, 'sort', 'featured_rank'),
+                self.getParam(taskDetail, 'countryCode', ),
+                self.getParam(taskDetail, 'zipCode', ''),
+                self.getParam(taskDetail, 'language'))
 
-    @retry_by_msg(retry_time=5)
-    def getGoodsByKeyword(self, keyword, page, sort, country_code, zip_code, language):
+    def getGoodsByKeyword(
+            self,
+            keyword,
+            page,
+            sort,
+            country_code,
+            zip_code,
+            language):
         '''
         搜索商品 (requests)
         :param keyword: 关键词
@@ -47,56 +74,61 @@ class AmazonCrawler:
         sort = self.cleanArgs(sort, 'sort')
         country_code = self.cleanArgs(country_code, 'country_code')
         language = self.cleanArgs(language, 'language')
-
         # url = 'https://www.amazon.com/s?k={keyword}&s={sort}&page={page}&language={language}&qid={qid}'.format(keyword=keyword, sort=sort, page=page,language=language,qid=int(time.time()))
         # url = 'https://www.amazon.com/s/ref=nb_sb_noss_1?url=search-alias%3Daps&field-keywords={keyword}&page={page}&s={sort}&language={language}'.format(
         #     keyword=keyword, page=page, sort=sort, language=language)
-        url ='https://www.amazon.com/s?k={keyword}&s={sort}&page={page}&language={language}&ref=nb_sb_noss_1'.format(
+        url = 'https://api.crawlbase.com/?token=g5eankdfZ8v8utNjJyzJvw&url=https://www.amazon.com/s?k={keyword}&s={sort}&page={page}&language={language}&ref=nb_sb_noss_1'.format(
             keyword=keyword, page=page, sort=sort, language=language)
-        print(url)
+        print(f"url:{url}")
+        payload = {}
         headers = {
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-            'accept-encoding': 'gzip, deflate, br',
-            'accept-language': 'en-us,us;q=0.9',
-            'referer': 'https://www.amazon.com/ref=nav_logo',
-            'user-agent': random.choice(agents)
         }
-        try:
-            self.session = self.get_session() if self.session is None else self.session
-            if country_code != 'US':
-                self.session = self.get_country_session(self.session, country_code)
-            elif zip_code != '':
-                self.session = self.get_zipcode_session(self.session, zip_code)
-            result = func_timeout(self.request_time_out, lambda: self.session.get(url, headers=headers))
-            html_doc = result.text
-            print(result.status_code)
-            res = self.check_is_limited(html_doc)
-            if res is not None:
-                self.session = None
-                return res
-
+        response = requests.request("GET", url, headers=headers, data=payload,timeout=(90,90))
+        # self.session = self.get_session() if self.session is None else self.session
+        # if country_code != 'US':
+        #     self.session = self.get_country_session(self.session, country_code)
+        # elif zip_code != '':
+        #     self.session = self.get_zipcode_session(self.session, zip_code)
+        # result = func_timeout(self.request_time_out, lambda: self.session.get(url, headers=headers))
+        print(f"code:{response.status_code}")
+        # print(response.text)
+        if response.status_code==200:
+            print("成功")
+            html_doc = response.text
+            now = datetime.datetime.now()
+            nowdatetime = now.strftime("%Y-%m-%d")
+            fw = open(
+                f'E:/{nowdatetime}/{keyword}-{page}.html',
+                'w',
+                encoding='utf-8')
+            fw.write(str(html_doc))
+            fw.close()
             result = self.dataCleaner.goodsList(html_doc)
             return result
-
-        except exceptions.FunctionTimedOut:
-            res['code'] = 401
-            res['msg'] = "请求超时"
-            return res
-
-        except:
-            res['code'] = 401
-            res['msg'] = "请求出错"
-            # res['msg'] = str(traceback.format_exc())
-            return res
+        else:
+            print("失败")
+            self.getGoodsByKeyword(keyword,
+            page,
+            sort,
+            country_code,
+            zip_code,
+            language)
+        # res['msg'] = str(traceback.format_exc())
 
     def getCode(self, url):
         ct = requests.get(url, verify=False).content
 
-        result = requests.post(url='http://192.168.0.123:5658', data=ct, verify=False).text
+        result = requests.post(
+            url='http://192.168.0.123:5658',
+            data=ct,
+            verify=False).text
         if result and len(result):
             return result
         else:
-            result = requests.post(url='http://192.168.0.200:5658', data=ct, verify=False).text
+            result = requests.post(
+                url='http://192.168.0.200:5658',
+                data=ct,
+                verify=False).text
             return result
 
     def getParam(self, taskDetail, param, default=None):
@@ -128,13 +160,17 @@ class AmazonCrawler:
             else:
                 try:
                     result = int(data)
-                except:
+                except BaseException:
                     result = 1
 
         elif type == 'sort':
             default_sort = 'featured_rank'
-            if not data or data not in ['featured_rank', 'price-asc-rank', 'price-desc-rank', 'review-rank',
-                                        'date-desc-rank']:
+            if not data or data not in [
+                'featured_rank',
+                'price-asc-rank',
+                'price-desc-rank',
+                'review-rank',
+                    'date-desc-rank']:
                 result = default_sort
             else:
                 result = data
@@ -200,7 +236,7 @@ class AmazonCrawler:
         }
         try:
             resp = session.post(url, headers=headers, data=post_data)
-        except:
+        except BaseException:
             pass
 
         return session
@@ -236,17 +272,214 @@ class AmazonCrawler:
         return session
 
     def test(self):
-        taskDetail1 = {'type': 'amazonKeyword', 'keyword': 'cable railing', 'sort': 'featured_rank', 'page': 2,
-                       'countryCode': 'US', 'zipCode': '', 'language': 'en_US'}
+        select_sql = """
+            select keyword from keywordlist
+        """
+        cur.execute(select_sql)
+        rest = cur.fetchall()
+        db.commit()
+        print(rest)
+        now = datetime.datetime.now()
+        nowdatetime = now.strftime("%Y-%m-%d")
+        if os.path.exists(f"E:/{nowdatetime}/") == False:
+            os.mkdir(f"E:/{nowdatetime}/")
+        for solokeyword in rest:
+            itemlist = []
+            adlist = []
+            keyword = solokeyword[0]
+            print(keyword)
+            item_No = 1
+            aditem_No = 1
+            for page in range(1, 4):
+                taskDetail1 = {
+                    'type': 'amazonKeyword',
+                    'keyword': f'{keyword}',
+                    'sort': 'featured_rank',
+                    'page': f'{page}',
+                    'countryCode': 'US',
+                    'zipCode': '',
+                    'language': 'en_US'}
+                result = self.request(taskDetail1)
+                json.dumps(result)
+                print(result)
+                new_itemlist = result['data'][0]['itemlist']
+                new_aditemlist = result['data'][0]['adlist']
+                itemlist = itemlist + new_itemlist
+                adlist = adlist + new_aditemlist
+            allrangking=len(itemlist)
+            adallranking =len(adlist)
+            for soloitem in itemlist:
+                itemid = soloitem['item_id']
+                IS = soloitem['IS']
+                insert_sql = f"""
+                INSERT INTO keywordranking(datetime,keyword,ASIN,ranking,YOR,allranking) values ('{nowdatetime}','{keyword}','{itemid}',{item_No},{IS},{allrangking})
+                """
+                item_No = item_No + 1
+                print(insert_sql)
+                cur.execute(insert_sql)
+                db.commit()
+                print("插入成功")
+            for adsoloitem in adlist:
+                select_sql = f"""
+                    select * from asin where ASIN ="{adsoloitem}"
+                """
+                cur.execute(select_sql)
+                db.commit()
+                rest = cur.fetchall()
+                db.commit()
 
-        result = self.request(taskDetail1)
-        print(json.dumps(result, ensure_ascii=False, indent=4))
+                if len(rest) == 0:
+                    IS = 0
+                else:
+                    IS = 1
+                insert_sql = f"""
+                INSERT INTO adkeywordranking(datetime,keyword,ASIN,ranking,YOR,allranking) values ('{nowdatetime}','{keyword}','{adsoloitem}',{aditem_No},{IS},{adallranking})
+                """
+                aditem_No = aditem_No + 1
+                print(insert_sql)
+                cur.execute(insert_sql)
+                db.commit()
+                print("插入成功")
+        self.rate()
 
+    def rate(self):
+        keyword_select_sql = """
+        select * from keywordlist
+        """
+        cur.execute(keyword_select_sql)
+        keyword_select_sql_res = cur.fetchall()
+        db.commit()
+        now = datetime.datetime.now()
+        nowdatetime = now.strftime("%Y-%m-%d")
+        for solokeyword_inf in keyword_select_sql_res:
+            solokeyword = solokeyword_inf[2]
+            get_keyword_list = f"""
+            select * from keywordranking where keyword= '{solokeyword}' And datetime='{nowdatetime}'
+            """
+            get_keyword_list_myinf = f"""
+            select * from keywordranking where keyword= '{solokeyword}' AND YOR=1 AND datetime='{nowdatetime}'
+            """
+            cur.execute(get_keyword_list)
+            get_keyword_list_res = cur.fetchall()
+            db.commit()
+            all_num = len(get_keyword_list_res)
+            cur.execute(get_keyword_list_myinf)
+            get_keyword_list_myinf_res = cur.fetchall()
+            db.commit()
+            yes_num = len(get_keyword_list_myinf_res)
+            try:
+                nature_rate = round(yes_num / all_num * 100, 2)
+            except ZeroDivisionError:
+                nature_rate =0
+            get_keyword_list = f"""
+                        select * from adkeywordranking where keyword= '{solokeyword}' And datetime='{nowdatetime}'
+                        """
+            get_keyword_list_myinf = f"""
+                        select * from adkeywordranking where keyword= '{solokeyword}' AND YOR=1 AND datetime='{nowdatetime}'
+                        """
+            cur.execute(get_keyword_list)
+            get_keyword_list_res = cur.fetchall()
+            db.commit()
+            all_num = len(get_keyword_list_res)
+            cur.execute(get_keyword_list_myinf)
+            get_keyword_list_myinf_res = cur.fetchall()
+            db.commit()
+            yes_num = len(get_keyword_list_myinf_res)
+            try:
+                ad_rate = round(yes_num / all_num * 100, 2)
+            except ZeroDivisionError:
+                ad_rate =0
+            print(f"""{solokeyword}:自然排名：{nature_rate}%，广告占比：{ad_rate}%""")
+            insert_sql = f"""
+            Insert into keyword_rate(datetime, keyword, nature_rate, ad_rate) VALUES ('{nowdatetime}','{solokeyword}','{nature_rate}','{ad_rate}')
+            """
+            cur.execute(insert_sql)
+            db.commit()
 
+    def test2(self):
+        select_sql = """
+            select keyword from keywordlist
+        """
+        cur.execute(select_sql)
+        rest = cur.fetchall()
+        db.commit()
+        print(rest)
+        now = datetime.datetime.now()
+        nowdatetime = now.strftime("%Y-%m-%d")
+        if os.path.exists(f"E:/{nowdatetime}/") == False:
+            os.mkdir(f"E:/{nowdatetime}/")
+        for solokeyword in rest:
+            itemlist = []
+            adlist = []
+            keyword = solokeyword[0]
+            keyword=keyword.replace(" ","+")
+            print(keyword)
+            item_No = 1
+            aditem_No = 1
+            for page in range(1, 4):
+                # time.sleep(random.uniform(1,4))
+                result =get_selenium_amazonkeyword(keyword,page)
+                print(type(result))
+                json.dumps(result)
+                print(type(result))
+                new_itemlist = result['data'][0]['itemlist']
+                new_aditemlist = result['data'][0]['adlist']
+                itemlist = itemlist + new_itemlist
+                adlist = adlist + new_aditemlist
+            allrangking=len(itemlist)
+            adallranking =len(adlist)
+            for soloitem in itemlist:
+                itemid = soloitem['item_id']
+                IS = soloitem['IS']
+                insert_sql = f"""
+                INSERT INTO keywordranking(datetime,keyword,ASIN,ranking,YOR,allranking) values ('{nowdatetime}','{keyword}','{itemid}',{item_No},{IS},{allrangking})
+                """
+                item_No = item_No + 1
+                print(insert_sql)
+                cur.execute(insert_sql)
+                db.commit()
+                print("插入成功")
+            for adsoloitem in adlist:
+                select_sql = f"""
+                    select * from asin where ASIN ="{adsoloitem}"
+                """
+                cur.execute(select_sql)
+                db.commit()
+                rest = cur.fetchall()
+                db.commit()
 
+                if len(rest) == 0:
+                    IS = 0
+                else:
+                    IS = 1
+                insert_sql = f"""
+                INSERT INTO adkeywordranking(datetime,keyword,ASIN,ranking,YOR,allranking) values ('{nowdatetime}','{keyword}','{adsoloitem}',{aditem_No},{IS},{adallranking})
+                """
+                aditem_No = aditem_No + 1
+                print(insert_sql)
+                cur.execute(insert_sql)
+                db.commit()
+                print("插入成功")
+        self.rate()
 
+    def get_ip(self):
+        response = requests.get(
+            url="http://httpbin.org/ip",
+            proxies={"http": "http://g5eankdfZ8v8utNjJyzJvw:@smartproxy.crawlbase.com:8012",
+                     "https": "http://g5eankdfZ8v8utNjJyzJvw:@smartproxy.crawlbase.com:8012"},
+            verify=False
+        )
+        print('Response Code: ', response.status_code)
+        print('Response Body: ', response.text)
+        result= json.loads(response.text)
+        ip = result['origin']
+        proxies = {'http': f'http://{ip}',
+                   'https': f'http://{ip}'}
+        return proxies
 
 if __name__ == '__main__':
     crawler = AmazonCrawler()
-    count = 1
-    crawler.test()
+    crawler.rate()
+    # scheduler =BlockingScheduler()
+    # scheduler.add_job(crawler.test,'cron', month="*",day_of_week= "3", hour="9")
+    # scheduler.start()
